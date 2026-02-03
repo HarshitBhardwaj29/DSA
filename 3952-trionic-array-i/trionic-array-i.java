ae6b21b6d6adc0f0a0dc013a42d121353ce03fcd
Harshit Bhardwaj <bhardwajharsh1984@gmail.com>
2026-02-03 08:26:03 +0530

    Time: 1 ms (72.27%) | Memory: 44.5 MB (81.51%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.java
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -1,31 +1,24 @@
 class Solution {
     public boolean isTrionic(int[] nums) {
         int n = nums.length;
-        if (n < 3) return false;
-        boolean p = false; 
+        if(n<=3)return false;
+        boolean p = false;
         boolean q = false;
-        if (nums[1] <= nums[0]) return false;
-
-        for (int i = 1; i < n; i++) {
+         if(nums[0]>nums[1])return false;
+        for(int i=1;i<n;i++){
             if (nums[i] == nums[i - 1]) return false;
-
-            if (!p && !q) { 
-                if (nums[i] < nums[i - 1]) {
-                    p = true;
-                }
-            } 
-            else if (p && !q) { 
-                if (nums[i] > nums[i - 1]) {
-                    q = true;
-                }
-            } 
-            else { 
-                if (nums[i] < nums[i - 1]) {
+            if(nums[i-1]>nums[i] && !p && !q){
+                p=true;
+            }
+            else if(q==false && p==true && nums[i]>nums[i-1]){
+                q=true;
+            }
+            else if(q==true && p==true){
+                if(nums[i]<nums[i-1]){
                     return false;
                 }
             }
         }
-
-        return p && q; 
+        return (p&&q);
     }
 }
