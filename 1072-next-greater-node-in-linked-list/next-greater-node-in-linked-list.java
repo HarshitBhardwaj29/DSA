5a1837b58a861f4c83b1b8a271eb76498c7a300c
Harshit Bhardwaj <bhardwajharsh1984@gmail.com>
2026-02-06 11:00:46 +0530

    Time: 12 ms (95.08%) | Memory: 49.6 MB (61.63%) - LeetSync

--- 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
+++ 1072-next-greater-node-in-linked-list/next-greater-node-in-linked-list.java
@@ -10,40 +10,42 @@
  */
 class Solution {
     public ListNode reverse(ListNode head){
-        if(head==null)return null;
+        if(head==null) return null;
         ListNode prev = null;
         ListNode curr = head;
         while(curr!=null){
             ListNode temp = curr.next;
-            curr.next=prev;
+            curr.next = prev;
             prev = curr;
-            curr=temp;
+            curr = temp;
         }
         return prev;
     }
+
     public int length(ListNode head){
-        if(head==null)return 0;
         int l = 0;
         ListNode curr = head;
         while(curr!=null){
-            curr=curr.next;
+            curr = curr.next;
             l++;
         }
         return l;
     }
+
     public int[] nextLargerNodes(ListNode head) {
         int len = length(head);  
         ListNode curr = reverse(head);
-        Stack<Integer>st = new Stack<>();
-        int z=len-1;
+
+        Deque<Integer> st = new ArrayDeque<>();
+        int z = len - 1;
         int[] ans = new int[len];
-        while(curr!=null){
-            while(!st.isEmpty() && st.peek()<=curr.val){
+
+        while(curr != null){
+            while(!st.isEmpty() && st.peek() <= curr.val){
                 st.pop();
             }
             ans[z--] = st.isEmpty() ? 0 : st.peek();
             st.push(curr.val);
-
             curr = curr.next;
         }
         return ans;
