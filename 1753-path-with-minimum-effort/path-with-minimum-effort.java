e62f28bfc2640542e664cdb987580fe7bcee72a5
Harshit Bhardwaj <bhardwajharsh1984@gmail.com>
2026-03-17 11:26:00 +0530

    Time: 40 ms (74.34%) | Memory: 48 MB (37.13%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.java
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -1,47 +1,41 @@
 import java.util.*;
 
 class Pair {
-    int x, y, dist;
-    Pair(int x, int y, int dist) {
+    int x, y, dis;
+    Pair(int x, int y, int dis) {
         this.x = x;
         this.y = y;
-        this.dist = dist;
+        this.dis = dis;
     }
 }
 
 class Solution {
     public int minimumEffortPath(int[][] heights) {
-
         int n = heights.length;
         int m = heights[0].length;
-
         int[][] dist = new int[n][m];
-        for(int i = 0; i < n; i++)
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b) -> a.dist - b.dist);
-        pq.add(new Pair(0,0,0));
+        for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.dis - b.dis);
+        pq.add(new Pair(0, 0, 0));
         dist[0][0] = 0;
-
-        int[] dx = {0,1,-1,0};
-        int[] dy = {1,0,0,-1};
-
-        while(!pq.isEmpty()) {
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, 1, 0, -1};
+        while (!pq.isEmpty()) {
             Pair p = pq.poll();
-            if(p.x == n-1 && p.y == m-1)return p.dist;
-            for(int i = 0; i < 4; i++) {
-                int nx = p.x + dx[i];
-                int ny = p.y + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    int effort = Math.max(p.dist,Math.abs(heights[p.x][p.y] - heights[nx][ny]));
-                    if(effort < dist[nx][ny]) {
-                        dist[nx][ny] = effort;
-                        pq.add(new Pair(nx, ny, effort));
+            int x = p.x, y = p.y, d = p.dis;
+            if (x == n - 1 && y == m - 1) return d;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
+                    int newEffort = Math.max(d, Math.abs(heights[x][y] - heights[nx][ny]));
+                    if (newEffort < dist[nx][ny]) {
+                        dist[nx][ny] = newEffort;
+                        pq.add(new Pair(nx, ny, newEffort));
                     }
                 }
             }
         }
-
         return 0;
     }
 }
